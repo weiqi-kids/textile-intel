012b49704dbf1c43b325dda45229a69b11fdbabb
Light <lightman.chang@gmail.com>
2026-04-26 11:08:40 +0800

    feat: add Playwright fetchers for all tracked companies

--- fetchers/__init__.py
+++ fetchers/__init__.py
@@ -1,6 +1,43 @@
 """
-Company fetcher registry.
-Add Playwright-based fetchers here as they are implemented.
+公司新聞爬蟲
 """
 
-FETCHERS = {}
+from .base import CompanyFetcher, CompanyDocument
+
+from .adidas import AdidasFetcher
+from .eclat import EclatFetcher
+from .far_eastern import FarEasternFetcher
+from .fast_retailing import FastRetailingFetcher
+from .formosa_taffeta import FormosaTaffetaFetcher
+from .hm import HmFetcher
+from .inditex import InditexFetcher
+from .lealea import LealeaFetcher
+from .lenzing import LenzingFetcher
+from .lululemon import LululemonFetcher
+from .makalot import MakalotFetcher
+from .nike import NikeFetcher
+from .pvh import PvhFetcher
+from .shenzhou import ShenzhouFetcher
+from .tapestry import TapestryFetcher
+from .toray import TorayFetcher
+from .vf_corp import VfCorpFetcher
+
+FETCHERS = {
+    "adidas": AdidasFetcher,
+    "eclat": EclatFetcher,
+    "far_eastern": FarEasternFetcher,
+    "fast_retailing": FastRetailingFetcher,
+    "formosa_taffeta": FormosaTaffetaFetcher,
+    "hm": HmFetcher,
+    "inditex": InditexFetcher,
+    "lealea": LealeaFetcher,
+    "lenzing": LenzingFetcher,
+    "lululemon": LululemonFetcher,
+    "makalot": MakalotFetcher,
+    "nike": NikeFetcher,
+    "pvh": PvhFetcher,
+    "shenzhou": ShenzhouFetcher,
+    "tapestry": TapestryFetcher,
+    "toray": TorayFetcher,
+    "vf_corp": VfCorpFetcher,
+}
---
+++ fetchers/fast_retailing.py
@@ -0,0 +1,85 @@
+"""
+迅銷集團 官網爬蟲
+"""
+
+from datetime import datetime
+from typing import Optional
+import logging
+
+from .base import CompanyFetcher, CompanyDocument
+
+logger = logging.getLogger(__name__)
+
+
+class FastRetailingFetcher(CompanyFetcher):
+    """迅銷集團 爬蟲"""
+
+    company_id = "fast_retailing"
+    company_name = "迅銷集團"
+    news_url = "https://www.迅銷集團.com/news"
+
+    def parse_news(self, html: str) -> list[CompanyDocument]:
+        soup = self._parse_html(html)
+        documents = []
+        selectors = [
+            "article", "[class*='press']", "[class*='news-item']",
+            "[class*='release']", ".media-item", ".post-item", "li.item",
+        ]
+        articles = []
+        for sel in selectors:
+            articles = soup.select(sel)
+            if articles:
+                break
+        for article in articles[:30]:
+            title_elem = article.select_one("h2 a, h3 a, h4 a, a.title, .title a")
+            if not title_elem:
+                heading = article.select_one("h2, h3, h4")
+                link = article.select_one("a[href]")
+                if heading and link:
+                    title = heading.get_text(strip=True)
+                    url = link.get("href", "")
+                elif link:
+                    title = link.get_text(strip=True)
+                    url = link.get("href", "")
+                else:
+                    continue
+            else:
+                title = title_elem.get_text(strip=True)
+                url = title_elem.get("href", "")
+            if not title or len(title) < 5:
+                continue
+            if url and not url.startswith("http"):
+                url = "https://www.迅銷集團.com" + url
+            date_elem = article.select_one("time, .date, [class*='date']")
+            published_at = None
+            if date_elem:
+                dt_str = date_elem.get("datetime") or date_elem.get_text(strip=True)
+                if dt_str:
+                    try:
+                        published_at = datetime.fromisoformat(dt_str[:10])
+                    except (ValueError, TypeError):
+                        pass
+            doc = CompanyDocument(
+                company_id=self.company_id,
+                doc_type="news",
+                title=title,
+                url=url,
+                published_at=published_at,
+                language="en",
+                tags=["news"],
+            )
+            documents.append(doc)
+        logger.info(f"Parsed {len(documents)} news from 迅銷集團")
+        return documents
+
+    def parse_ir(self, html: str) -> list[CompanyDocument]:
+        return []
+
+
+if __name__ == "__main__":
+    fetcher = FastRetailingFetcher()
+    result = fetcher.fetch_all()
+    for doc_type, docs in result.items():
+        print(f"{doc_type}: {len(docs)} documents")
+        for doc in docs[:5]:
+            print(f"  [{doc.published_at}] {doc.title[:80]}")
